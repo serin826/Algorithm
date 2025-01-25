7d91f761152e54a96ac2b23a3fd7cccae563720d
serin826 <sr.jeong@outlook.kr>
2025-01-25 16:57:38 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -2,11 +2,10 @@ A, B = map(int, input().split())
 C = int(input())
 
 time = A * 60 + B + C
+
 H = time // 60
 M = time % 60
 
-if H >= 24:
-    H = H % 24
-    print(H, M)
-else:
-    print(H, M)
+H %= 24
+
+print(H, M)
